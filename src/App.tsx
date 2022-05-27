8b2261316946a718e5af6d2031fd532c9fc06141
Hrva <harwey.ch@gmail.com>
2022-05-27 13:07:41 +0200

    override webpack config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import mqtt from "mqtt";
+import "./redefineBuffer";
 
 function App() {
+  useEffect(() => {
+    const mqttTest = mqtt.connect("ws://212.39.114.103:9001/", {
+      username: "komore_main",
+      password: "mci_neuros_2020",
+    });
+    console.log(mqttTest);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/redefineBuffer.ts
@@ -0,0 +1,5 @@
+import process from "process";
+import buffer from "buffer";
+
+(window as any).Buffer = buffer.Buffer;
+(window as any).process = process;
